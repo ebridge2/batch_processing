00d3e7b1d2dfe60bd2e3721ef8647a05a8338bc4
Eric Bridgeford <ebridge2@jhu.edu>
2024-11-18 08:01:30 -0500

    updated job logging script

--- job_logging.sh
+++ job_logging.sh
@@ -3,14 +3,16 @@
 # Start the monitoring in background, using the container's own PID as the log name
 (
     while true; do
-        echo "$(date '+%Y-%m-%d %H:%M:%S'),$(ps -p 1 -o %cpu,%mem | tail -1),$(df -h / | tail -1 | awk '{print $5}')" >> resource_usage.log
+        echo "$(date '+%Y-%m-%d %H:%M:%S'),$(ps -p 1 -o %cpu,%mem | tail -1),$(df -h / | tail -1 | awk '{print $5}')" >> /batch/resource_usage.log
         sleep 1
     done
 ) &
 MONITOR_PID=$!
 
+cd /code
 # Run your actual command
-cpac run --pipeline-file=pipeline_config_batcheffects.yml /data/inputs /data/outputs participant
+cpac run --pipeline-file=/batch/pipeline_config_batcheffects.yml /data/inputs /data/outputs participant
 
+cd -
 # When the command finishes, stop the monitoring
 kill $MONITOR_PID
